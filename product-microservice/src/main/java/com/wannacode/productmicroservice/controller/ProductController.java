9185ab2b910b47bebf0c7824941de325aa49dab9
marcos-guillen <marcos_guillen@hotmail.com>
2022-10-08 21:29:35 +0200

    video 7

--- product-microservice/src/main/java/com/wannacode/productmicroservice/controller/ProductController.java
+++ product-microservice/src/main/java/com/wannacode/productmicroservice/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.ResponseEntity;
 
 import com.wannacode.productmicroservice.repository.ProductRepository;
 import com.wannacode.productmicroservice.entity.ProductEntity;
